702d3e091185e5f830eab7777ecaa377e61d2c0c
rrysbekov7 <blackfox.97@bk.ru>
2025-03-09 03:34:58 +0600

    First Circle-Task commit

--- task-2/index.js
+++ task-2/index.js
@@ -1,36 +1,31 @@
 document.addEventListener('DOMContentLoaded', event => {
-  let chessBoard = document.createElement('div');
-  chessBoard.className = 'chess__container';
-  document.body.appendChild(chessBoard);
-  chessBoard.style.margin = '50px auto';
-  chessBoard.style.display = 'flex';
-  chessBoard.style.flexWrap = 'wrap';
+  const circleFiled = document.createElement('div');
+  circleFiled.style.width = '1024px';
+  circleFiled.style.height = '768px';
+  circleFiled.style.outline = '1px solid black';
+  circleFiled.style.position = 'relative';
+  circleFiled.style.overflow = 'hidden';
+  circleFiled.style.margin = '0 auto';
+  document.body.appendChild(circleFiled);
 
-  const myChess = (width, height, color, blockWidth) => {
-    chessBoard.style.width = `${width * blockWidth}px`;
-    chessBoard.style.outline = `1px solid ${color}`;
-    for (let i = 0; i < height; i++) {
-      for (let j = 0; j < width; j++) {
-        const chessBox = document.createElement('div');
-        chessBox.style.width = `${blockWidth}px`;
-        chessBox.style.height = `${blockWidth}px`;
-        chessBox.style.outline = `1px solid ${color}`;
-        chessBox.style.background = 'white';
-        chessBoard.appendChild(chessBox);
-        if (i % 2 == 0) {
-          if (j % 2 == 0) {
-            chessBox.style.background = color;
-          }
-        } else {
-          if (j % 2 != 0) {
-            chessBox.style.background = color;
-          }
-        }
+  const drawCircles = (count) => {
+    const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'pink', 'brown', 'orange', 'grey', 'black', 'cyan', 'crimson', 'springgreen', 'violet', 'olive'];
+    for (let i = 0; i < count; i++) {
+      const circleElement = document.createElement('div');
+      circleElement.style.position = 'absolute';
+      circleElement.style.borderRadius = '50%';
+      let circleWidth = Math.round(Math.random() * 200);
+      if (circleWidth < 50) {
+        circleWidth += 50;
       }
+      circleElement.style.width = `${circleWidth}px`;
+      circleElement.style.height = `${circleWidth}px`;
+      circleElement.style.background = `${colors[Math.round(Math.random() * 15)]}`;
+      circleElement.style.top = `${Math.round(Math.random() * 768)}px`;
+      circleElement.style.right = `${Math.round(Math.random() * 1024)}px`;
+      circleFiled.appendChild(circleElement);
     }
   }
-
-  myChess(16, 16, 'green', 20);
-
+  drawCircles(20);
   event.preventDefault();
 });
