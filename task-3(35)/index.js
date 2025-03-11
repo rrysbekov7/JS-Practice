b23aa238f11f095da7a39afa19eaf74c4923d3cc
rrysbekov7 <blackfox.97@bk.ru>
2025-03-12 00:48:18 +0600

    Interval number element commit

--- task-3(35)/index.js
+++ task-3(35)/index.js
@@ -1,11 +1,38 @@
 document.addEventListener('DOMContentLoaded', event => {
   const container = document.getElementById('container');
-  const btn = document.getElementById('add-item-btn');
-  btn.addEventListener('click', e => {
+  container.style.border = '1px solid blue';
+  container.style.padding = '10px';
+  container.style.marginBottom = '10px';
+  container.style.borderRadius = '5px';
+  const startBtn = document.getElementById('start');
+  const stopBtn = document.getElementById('stop');
+  startBtn.style.display = 'inline-block';
+  startBtn.style.background = 'lightblue';
+  startBtn.style.borderRadius = '3px';
+  startBtn.style.padding = '10px 30px';
+  startBtn.style.border = '1px solid grey';
+  stopBtn.style.display = 'inline-block';
+  stopBtn.style.background = 'lightblue';
+  stopBtn.style.borderRadius = '3px';
+  stopBtn.style.padding = '10px 30px';
+  stopBtn.style.border = '1px solid grey';
+
+
+  const newNumElementFunction = () => {
+    const number = Math.round(Math.random() * 20);
     const element = document.createElement('div');
     element.className = 'element';
-    element.innerText = 'Element';
+    element.innerText = number;
     container.appendChild(element);
+  }
+
+  startBtn.addEventListener('click', () => {
+    const interval = setInterval(newNumElementFunction, 3000);
+
+    stopBtn.addEventListener('click', () => {
+      clearInterval(interval);
+    });
   });
+
   event.preventDefault();
 });
