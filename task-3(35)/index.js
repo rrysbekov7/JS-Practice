206cb124026e356132c1256d969aef46678084eb
rrysbekov7 <blackfox.97@bk.ru>
2025-03-11 22:36:03 +0600

    Element event commit

--- task-3(35)/index.js
+++ task-3(35)/index.js
@@ -1,1 +1,11 @@
-
+document.addEventListener('DOMContentLoaded', event => {
+  const container = document.getElementById('container');
+  const btn = document.getElementById('add-item-btn');
+  btn.addEventListener('click', e => {
+    const element = document.createElement('div');
+    element.className = 'element';
+    element.innerText = 'Element';
+    container.appendChild(element);
+  });
+  event.preventDefault();
+});
