2015026e63aea16a592558f13aecf55a08f243d5
rrysbekov7 <blackfox.97@bk.ru>
2025-03-14 06:26:23 +0600

    Timer commit

--- task-3(35)/index.js
+++ task-3(35)/index.js
@@ -1,38 +1,69 @@
 document.addEventListener('DOMContentLoaded', event => {
-  const container = document.getElementById('container');
-  container.style.border = '1px solid blue';
-  container.style.padding = '10px';
-  container.style.marginBottom = '10px';
-  container.style.borderRadius = '5px';
+  let container = document.getElementsByTagName('div');
+  console.log(container[0].innerText);
+  container[0].style.border = '1px solid red';
+  container[0].style.borderRadius = '5px';
+  container[0].style.marginBottom = '10px';
+  const minutes = document.getElementById('minutes');
+  const seconds = document.getElementById('seconds');
   const startBtn = document.getElementById('start');
-  const stopBtn = document.getElementById('stop');
-  startBtn.style.display = 'inline-block';
-  startBtn.style.background = 'lightblue';
-  startBtn.style.borderRadius = '3px';
-  startBtn.style.padding = '10px 30px';
-  startBtn.style.border = '1px solid grey';
-  stopBtn.style.display = 'inline-block';
-  stopBtn.style.background = 'lightblue';
-  stopBtn.style.borderRadius = '3px';
-  stopBtn.style.padding = '10px 30px';
-  stopBtn.style.border = '1px solid grey';
-
-
-  const newNumElementFunction = () => {
-    const number = Math.round(Math.random() * 20);
-    const element = document.createElement('div');
-    element.className = 'element';
-    element.innerText = number;
-    container.appendChild(element);
-  }
+  const pauseBtn = document.getElementById('pause');
+  const resetBtn = document.getElementById('reset');
+  container[0].style.textAlign = 'center';
+  minutes.style.fontSize = '60px';
+  seconds.style.fontSize = '60px';
+  minutes.style.fontWeight = '700';
+  seconds.style.fontWeight = '700';
+  minutes.style.fontFamily = "Poppins";
+  seconds.style.fontFamily = "Poppins";
 
   startBtn.addEventListener('click', () => {
-    const interval = setInterval(newNumElementFunction, 3000);
+    let second = parseInt(seconds.innerText);
+    let minute = parseInt(minutes.innerText);
+    const count = () => {
+      console.log('this is num ' + second);
+      second++;
 
-    stopBtn.addEventListener('click', () => {
+
+      if (second < 10) {
+        seconds.innerText = "0" + second;
+      } else {
+        seconds.innerText = second;
+      }
+      if (second === 60) {
+        second = "00";
+        seconds.innerText = second;
+        minute++;
+        minutes.innerText = "0" + minute;
+      }
+
+      if (minute < 10) {
+        minutes.innerText = "0" + minute;
+      } else {
+        minutes.innerText = minute;
+      }
+
+      if (minute === 60) {
+        minute = "0";
+        minutes.innerText = "0" + minute;
+      }
+
+    }
+
+    const interval = setInterval(count, 100);
+    pauseBtn.addEventListener('click', () => {
+      clearInterval(interval);
+    });
+    resetBtn.addEventListener('click', () => {
+      minutes.innerText = "00";
+      seconds.innerText = "00";
       clearInterval(interval);
     });
   });
 
+
+
+  console.log(container[0]);
+  console.log(parseInt(seconds.innerText));
   event.preventDefault();
 });
