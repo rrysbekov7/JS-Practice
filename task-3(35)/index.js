6e392287898d69698a562639a50964695ce39b1e
rrysbekov7 <blackfox.97@bk.ru>
2025-03-15 23:49:42 +0600

    Log In second commit

--- task-3(35)/index.js
+++ task-3(35)/index.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', event => {
+  const username = document.querySelector('input[name = "username"]');
+  const password = document.querySelector('input[name = "password"]');
+  const btn = document.querySelector('input[type="submit"]');
+  let usercount;
+  let passcount;
+  username.addEventListener('keyup', e => {
+    console.log(e.currentTarget.value.length);
+    usercount = e.currentTarget.value;
+  });
+
+  password.addEventListener('keyup', e => {
+    console.log(e.currentTarget.value);
+    passcount = e.currentTarget.value;
+  });
+
+  console.log(usercount);
+  if (usercount.length >= 6) {
+    btn.disabled = false;
+  }
+
+
 
   event.preventDefault();
 });
