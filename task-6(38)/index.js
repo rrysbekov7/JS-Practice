ad44f320ad0040fdd32997ba275c7af94f2fc22c
rrysbekov7 <blackfox.97@bk.ru>
2025-04-04 19:13:52 +0600

    taks1-level2 img Commit

--- task-6(38)/index.js
+++ task-6(38)/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', e => {
+  const container = document.getElementById('container');
   const name = document.getElementById('name');
   const region = document.getElementById('region');
   const subregion = document.getElementById('subregion');
@@ -6,18 +7,22 @@ document.addEventListener('DOMContentLoaded', e => {
   const flag = document.getElementById('flag');
   const countryText = document.getElementById('country-text');
   const show = document.getElementById('show');
-
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener("load", function () {
-    console.log(JSON.parse(this.responseText));
-    name.innerText = JSON.parse(this.responseText)[0].name.common;
-    region.innerText = JSON.parse(this.responseText)[0].region;
-    subregion.innerText = JSON.parse(this.responseText)[0].subregion;
-    capital.innerText = JSON.parse(this.responseText)[0].capital;
-    flag.innerText = JSON.parse(this.responseText)[0].flag;
-  });
-
+  const img = document.createElement('img');
+  container.appendChild(img);
   show.addEventListener('click', e => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", function () {
+      console.log(JSON.parse(this.responseText));
+      name.innerText = JSON.parse(this.responseText)[0].name.common;
+      region.innerText = JSON.parse(this.responseText)[0].region;
+      subregion.innerText = JSON.parse(this.responseText)[0].subregion;
+      capital.innerText = JSON.parse(this.responseText)[0].capital;
+      flag.innerText = JSON.parse(this.responseText)[0].flag;
+      console.log(JSON.parse(this.responseText)[0].flags.png);
+      img.setAttribute('src', `${JSON.parse(this.responseText)[0].flags.png}`);
+      img.style.outline = '1px solid blue';
+      img.style.width = '150px';
+    });
     xhr.open("GET", `https://restcountries.com/v3.1/name/${countryText.value}`);
     xhr.send();
     e.preventDefault();
