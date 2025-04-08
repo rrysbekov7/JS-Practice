6537bf92389d0975e6cbb342dffe1202250802bc
rrysbekov7 <blackfox.97@bk.ru>
2025-04-09 02:43:26 +0600

    taks39-40 third Commit

--- task-7(39)/index.js
+++ task-7(39)/index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", ev => {
   const form = document.querySelector("form");
   const countryList = [];
   const getCountryList = JSON.parse(localStorage.getItem("countryLocalList"));
+  const getNumCount = localStorage.getItem("numCount");
   let count = 1;
   option.innerText = 'Выберите страну';
   option.disabled = true;
@@ -15,7 +16,6 @@ document.addEventListener("DOMContentLoaded", ev => {
   const xhr = new XMLHttpRequest();
   xhr.addEventListener("load", function () {
     if (getCountryList.length !== 0) {
-      console.log('All', getCountryList);
       for (let j = 0; j < getCountryList.length; j++) {
         countryList.push(getCountryList[j]);
         container.style.display = "flex";
@@ -26,8 +26,6 @@ document.addEventListener("DOMContentLoaded", ev => {
         newContainer.style.outline = "1px solid red";
         newContainer.style.position = "relative";
         newContainer.style.marginBottom = "10px";
-        // container.appendChild(newContainer);
-
         const removeBtn = document.createElement("button");
         removeBtn.classList.add("remove");
         removeBtn.innerText = "Remove";
@@ -37,9 +35,8 @@ document.addEventListener("DOMContentLoaded", ev => {
         removeBtn.style.border = "none";
         removeBtn.style.backgroundColor = "transparent";
         removeBtn.style.fontWeight = "bold";
-
+        count++
         const text = document.createElement("p");
-        // text.innerText = JSON.parse(this.responseText)[i].name.common;
         text.innerHTML = `Post #${getCountryList[j].count} at ${getCountryList[j].date}
           being in: ${getCountryList[j].name}
           <img src=${getCountryList[j].flags} style="width: 15px; height: 10px">`;
@@ -47,14 +44,11 @@ document.addEventListener("DOMContentLoaded", ev => {
         newContainer.append(text);
         newContainer.append(getCountryList[j].text);
         newContainer.append(removeBtn);
-
+        console.log(getCountryList);
         removeBtn.addEventListener("click", e => {
-          console.log('sss', getCountryList[j].name);
           if (confirm("Are you sure you want to delete?")) {
             for (let i = 0; i < countryList.length; i++) {
-              // console.log(countryList[i]);
               if (getCountryList[j].name === countryList[i].name) {
-                console.log('fff', countryList[i]);
                 newContainer.remove(newContainer);
                 countryList.splice(i,1);
                 const countryLocalList = localStorage.setItem("countryLocalList", JSON.stringify(countryList));
@@ -75,9 +69,7 @@ document.addEventListener("DOMContentLoaded", ev => {
       select.appendChild(option);
     }
     select.addEventListener('click', e => {
-      console.log(select.value);
     })
-    console.log(JSON.parse(this.responseText)[0]);
     form.addEventListener("submit", e => {{
       container.style.display = "flex";
       container.style.flexDirection = "column-reverse";
@@ -87,7 +79,6 @@ document.addEventListener("DOMContentLoaded", ev => {
       newContainer.style.outline = "1px solid red";
       newContainer.style.position = "relative";
       newContainer.style.marginBottom = "10px";
-      // container.appendChild(newContainer);
       const removeBtn = document.createElement("button");
       removeBtn.classList.add("remove");
       removeBtn.innerText = "Remove";
@@ -99,11 +90,9 @@ document.addEventListener("DOMContentLoaded", ev => {
       removeBtn.style.fontWeight = "bold";
       for (let i = 0; i < numbers; i++) {
         if (select.value === JSON.parse(this.responseText)[i].name.common) {
-          console.log(i + 1);
-
-          console.log('sdsd', JSON.parse(this.responseText)[i].flags.png);
+          count++
           const text = document.createElement("p");
-          // text.innerText = JSON.parse(this.responseText)[i].name.common;
+          const numCount = localStorage.setItem("numCount", count);
           text.innerHTML = `Post #${count} at ${date.value} being in: ${select.value}
           <img src=${JSON.parse(this.responseText)[i].flags.png} style="width: 15px; height: 10px">`;
           text.style.margin = '0';
@@ -115,18 +104,15 @@ document.addEventListener("DOMContentLoaded", ev => {
           countryItem.date = date.value;
           countryItem.text = textArea.value;
           countryItem.flags = JSON.parse(this.responseText)[i].flags.png;
-          countryItem.count = count++;
+          countryItem.count = count;
           countryList.push(countryItem);
         }
 
       }
       removeBtn.addEventListener("click", e => {
-        console.log('xxx', select.value);
         if (confirm("Are you sure you want to delete?")) {
           for (let i = 0; i < countryList.length; i++) {
-            // console.log(countryList[i]);
             if (select.value === countryList[i].name) {
-              console.log('www', countryList[i]);
               newContainer.remove(newContainer);
               countryList.splice(i,1);
               const countryLocalList = localStorage.setItem("countryLocalList", JSON.stringify(countryList));
@@ -135,8 +121,6 @@ document.addEventListener("DOMContentLoaded", ev => {
         }
         e.preventDefault();
       })
-
-      console.log('countryList', countryList);
       const countryLocalList = localStorage.setItem("countryLocalList", JSON.stringify(countryList));
       e.preventDefault();
     }})
