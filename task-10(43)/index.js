09bef60028248af8b335e129952c57c74385830d
rrysbekov7 <blackfox.97@bk.ru>
2025-04-16 23:26:21 +0600

    taks43 empty Commit

--- task-10(43)/index.js
+++ task-10(43)/index.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", e => {
+  <script>
+    class CoffeeMachine {
 
+  }
+
+    const firstCoffeeMachine = new CoffeeMachine();
+    console.log(firstCoffeeMachine);
+
+  </script>
   e.preventDefault();
 })
