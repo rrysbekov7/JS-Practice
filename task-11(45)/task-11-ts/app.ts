172976c327b82a1b21228105093e2b4b3eee1f79
rrysbekov7 <blackfox.97@bk.ru>
2025-04-27 00:09:32 +0600

    taks45  Commit

--- task-11(45)/task-11-ts/app.ts
+++ task-11(45)/task-11-ts/app.ts
@@ -1,6 +1,21 @@
-const root = document.getElementById('root');
+const root : HTMLElement | null = document.getElementById('root');
 const div = document.createElement('div');
 div.innerHTML = "Hello";
+
+const input = document.createElement('input');
+input.type = 'text';
+input.setAttribute('id', 'name');
+
+
+const button: HTMLButtonElement = document.createElement('button');
+button.type = 'button';
+button.innerHTML = 'Get name';
+button.addEventListener('click', () => {
+  alert(input.value);
+})
+
+div.appendChild(input);
+div.appendChild(button);
 
 if (root !== null) {
   root.appendChild(div);
